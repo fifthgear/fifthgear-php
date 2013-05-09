c47cb330d3aeae3628dfc4968a5d3c5be86de3ba
brandoncorbin <bcorbin@infifthgear.com>
2013-05-09 10:13:44 -0400

    Updating class documentation

--- Docs/html/search/all_66.js
+++ Docs/html/search/all_66.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['fifthgear',['FifthGear',['../class_fifth_gear.html',1,'FifthGear'],['../namespace_fifth_gear.html',1,'FifthGear']]],
-  ['fifthgear_2ephp',['fifthgear.php',['../fifthgear_8php.html',1,'']]]
+  ['fifthgear_2ephp',['fifthgear.php',['../fifthgear_8php.html',1,'']]],
+  ['fifth_20gear_20integration_20with_20php',['Fifth Gear Integration with PHP',['../md__r_e_a_d_m_e.html',1,'']]]
 ];
---
+++ Docs/html/search/pages_66.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['fifth_20gear_20integration_20with_20php',['Fifth Gear Integration with PHP',['../md__r_e_a_d_m_e.html',1,'']]]
+];
